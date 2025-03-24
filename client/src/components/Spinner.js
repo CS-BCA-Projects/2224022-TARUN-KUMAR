f80828a4272d38ff8fbe88f1263be9804a10cf59
Tarun kumar <tarun.dsvv@gmail.com>
2025-03-24 13:56:38 +0530

    Spinner.js modified

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -19,7 +19,7 @@ return (
     <div className="d-flex flex-column justify-content-center align-items-center"style={{height:"100vh"}}>
         <h1 className="Text-center">Please wait {count} second</h1>
   <div className="spinner-border" role="status">
-    <span className="visually-hidden">Loading...</span>
+    <span className="visually-hidden">Loading.....</span>
   </div>
 </div>
 
